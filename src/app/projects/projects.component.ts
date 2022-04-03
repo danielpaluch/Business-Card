c325afdf494dfb848386324b4df724329082c2aa
danielpaluch <danielpaluch37@gmail.com>
2022-04-03 14:59:49 +0200

    update

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -12,35 +12,35 @@ export class ProjectsComponent implements OnInit {
   projects: Array<Project> = [
     {
       title: "Quiz",
-      description: "Simple Quiz done by Angular with adding but without database.",
-      content: "It\'s one of my first Angular projects. In future I want to expand this with deleting, adding and overviewing current questions. I'm also thinking about adding new options of answer, like ichoose answer or input text.",
+      description: "Angular",
+      content: "It\'s one of my first Angular projects. Simple Quiz done by Angular with adding but without database. In future I want to expand this with deleting, adding and overviewing current questions. I'm also thinking about adding new options of answer, like ichoose answer or input text.",
       done: true,
       url: "https://github.com/danielpaluch/Angular_Quiz"
     },
     {
       title: "To-do list",
-      description: "To-do list with udemy tutorial.",
-      content: "It\'s simplic todo list. Made with Material UI. In future it will base on database.",
+      description: "Angular.",
+      content: "It\'s simple todo list. Made with Material UI. In future it will base on database.",
       done: true,
       url: "https://github.com/danielpaluch/Angular_TodoList"
     },
     {
       title: "TicTacToe",
-      description: "TicTacToe game with 2-player and AI mode.",
-      content: "TicTacToe with basic mechanics. Spend more time to make it look good and without any kind of bugs or errors.",
+      description: "Angular",
+      content: "TicTacToe with basic mechanics. I've spend more time to make it look good and without any kind of bugs or errors. You are allowed to play in 2-player and AI mode",
       done: true,
       url: "https://github.com/danielpaluch/Angular_KolkoKrzyzyk"
     },
     {
       title: "KeyFinder",
-      description: "KeyFinder in file done by C#.",
+      description: "C#",
       content: "Simplic KeyFinder in linked file, shows how much the key is used and in which lines it occurs.",
       done: true,
       url: "https://github.com/danielpaluch/C-_KeyFinder"
     },
     {
       title: "Blog with Log-In",
-      description: "Blog where you can post and read comments.",
+      description: "Angular, Firebase.",
       content: "It\'s not done yet. I\'m still working on it, while learning databases and how to make users their own accounts.",
       done: false,
     },
